b388589da3bb18f3a93a29c26a350bbb0b34339c
Ilamparidhi <ilamparidhianbu@gmail.com>
2026-07-01 23:22:07 +0530

    Update upload-kubescape.sh

--- scripts/upload-kubescape.sh
+++ scripts/upload-kubescape.sh
@@ -4,7 +4,7 @@ set -e
 curl -X POST \
 "$DEFECTDOJO_URL/api/v2/import-scan/" \
 -H "Authorization: Token $DD_TOKEN" \
--F "scan_type=Kubescape Scan" \
+-F "scan_type=Kubescape" \
 -F "engagement=$ENGAGEMENT_ID" \
 -F "active=true" \
 -F "verified=false" \
