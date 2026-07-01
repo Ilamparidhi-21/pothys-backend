272c70e90bf09f1b1ecc5adf9a80b3762bc86c1a
Ilamparidhi <ilamparidhianbu@gmail.com>
2026-07-01 22:31:22 +0530

    Update sonarqube.sh

--- scripts/sonarqube.sh
+++ scripts/sonarqube.sh
@@ -11,7 +11,7 @@ sonar-scanner \
   -Dsonar.projectKey=pothys-backend \
   -Dsonar.projectName=pothys-backend \
   -Dsonar.sources=. \
-  -Dsonar.host.url=$SONAR_HOST_URL \
+  -Dsonar.host.url=http://localhost:9000 \
   -Dsonar.token=$SONAR_TOKEN
 
 echo "SonarQube scan completed."
