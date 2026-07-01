51d674ebdb60aa8a40eceaac9d6ac30d4b707aa7
Ilamparidhi <ilamparidhianbu@gmail.com>
2026-07-02 00:16:41 +0530

    Update push.sh

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,15 +3,22 @@ set -e
 
 echo "Logging into ECR..."
 
-aws ecr get-login-password --region $AWS_REGION | \
-docker login --username AWS --password-stdin $ECR_REPO
+: "${AWS_REGION:?AWS_REGION is NOT set}"
+: "${ECR_REPO:?ECR_REPO is NOT set}"
+: "${IMAGE_TAG:?IMAGE_TAG is NOT set}"
+
+echo "Using ECR_REPO=$ECR_REPO"
+echo "Using IMAGE_TAG=$IMAGE_TAG"
+
+aws ecr get-login-password --region "$AWS_REGION" | \
+docker login --username AWS --password-stdin "$ECR_REPO"
 
 echo "Building image..."
 
-docker build -t $ECR_REPO:$IMAGE_TAG .
+docker build -t "$ECR_REPO:$IMAGE_TAG" .
 
 echo "Pushing image..."
 
-docker push $ECR_REPO:$IMAGE_TAG
+docker push "$ECR_REPO:$IMAGE_TAG"
 
 echo "Image pushed successfully."
